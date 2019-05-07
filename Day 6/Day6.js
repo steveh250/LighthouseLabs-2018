14edf871e5171c48618624d7a0750fe8c3d42d04
steveh250 <steve.harris@shaw.ca>
2019-05-06 17:29:32 -0700

    Create Day6.js

---
+++ Day 6/Day6.js
@@ -0,0 +1,46 @@
+// Setup the array so I can test this outside of the challenge framework
+const availableModules = [
+  { name: "life-support", size: 10, enabled : false, essential: true },
+  { name: "Module 2",     size: 20, enabled : true , essential: false},
+  { name: "propulsion",   size: 30, enabled : false, essential: true },
+  { name: "Module 4",     size: 40, enabled : true , essential: false},
+  { name: "Module 5",     size: 50, enabled : false, essential: true },
+];
+
+var ship = {
+  powerOn: false,
+  modules: [],
+  antenna: {
+    active: false
+  }
+};
+
+
+// Function to power on ship
+function powerOn() {
+  ship.powerOn = true;
+}
+
+// Count availableModules
+function countModules() {
+  return availableModules.length;
+}
+
+function loadModule(index) {
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+// Modularise function to find name passed in
+function findModuleIndex(moduleName) {
+  for (var i = 0; i < countModules(); i++) {
+    if (availableModules[i].name == moduleName) {
+      return i;
+    }
+  }
+}
+
+// Find the propulsion module
+loadModule(findModuleIndex('life-support'));
+loadModule(findModuleIndex('propulsion'));
+loadModule(findModuleIndex('navigation'));
